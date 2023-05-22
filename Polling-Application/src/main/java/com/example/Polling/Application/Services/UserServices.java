c1a0bf76e4be37d27a1413e292d1cc98620f1820
ayou6 <ayoxb.abbas@gmail.com>
2023-05-22 06:01:39 -0700

    editting the classes

--- Polling-Application/src/main/java/com/example/Polling/Application/Services/UserServices.java
+++ Polling-Application/src/main/java/com/example/Polling/Application/Services/UserServices.java
@@ -1,6 +1,8 @@
 package com.example.Polling.Application.Services;
-
+import org.springframework.stereotype.Service;
 import java.util.*;
+@Service
+
 public class UserServices {
 static Scanner scn = new Scanner(System.in);
     public void menu() {
@@ -12,6 +14,10 @@ static Scanner scn = new Scanner(System.in);
         int choice = scn.nextInt();
         switch (choice) {
             case 1:
+                System.out.println("user :");
+                String userInput = scn.next();
+                System.out.println("Enter Password");
+                String userPass = scn.next();
 
                 break;
             case 2:
